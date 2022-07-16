3d02bfb858fe4c2e3b31ad7cac398094489bdb2d
Leo Gaskin <leo.gaskin@le0.gs>
2022-07-16 13:36:05 +0200

    Fix initialization error message printing

--- main.c
+++ main.c
@@ -193,23 +193,23 @@ int main(int argc, char *argv[]) {
     }
 
     if (isOptionDebug && (rc = sqlite3_config(SQLITE_CONFIG_LOG, debugLogCallback, 0))) {
-        fprintf(stderr, "internal: setting debug: %s", sqlite3_errstr(rc));
+        fprintf(stderr, "internal: setting debug: %s\n", sqlite3_errstr(rc));
     } else if ((rc = sqlite3_initialize())) {
-        fprintf(stderr, "internal: initializing sqlite: %s", sqlite3_errstr(rc));
+        fprintf(stderr, "internal: initializing sqlite: %s\n", sqlite3_errstr(rc));
     } else if ((rc = sqlite3_open_v2(stringOptionDatabase, &db, FLAG_SQLITE_OPEN, 0))) {
-        fprintf(stderr, "internal: opening database: %s", sqlite3_errstr(rc));
+        fprintf(stderr, "internal: opening database: %s\n", sqlite3_errstr(rc));
     } else if (isOptionWipe &&
                (rc = sqlite3_db_config(db, SQLITE_DBCONFIG_RESET_DATABASE, 1, 0) ||
                      sqlite3_exec(db, "VACUUM", 0, 0, 0) ||
                      sqlite3_db_config(db, SQLITE_DBCONFIG_RESET_DATABASE, 0, 0))) {
-        fprintf(stderr, "internal: wiping database: %s", sqlite3_errstr(rc));
+        fprintf(stderr, "internal: wiping database: %s\n", sqlite3_errstr(rc));
     } else if (isOptionTrace &&
                (rc = sqlite3_trace_v2(db, FLAG_SQLITE_TRACE, traceLogCallback, 0))) {
-        fprintf(stderr, "internal: setting tracing: %s", sqlite3_errstr(rc));
+        fprintf(stderr, "internal: setting tracing: %s\n", sqlite3_errstr(rc));
     } else if ((rc = sqlite3_nadeko_init(db, &zErr, 0))) {
-        fprintf(stderr, "internal: initializing extension: %s", sqlite3_errmsg(db));
+        fprintf(stderr, "internal: initializing extension: %s\n", sqlite3_errmsg(db));
     } else if ((rc = sqlite3_lines_init(db, &zErr, 0))) {
-        fprintf(stderr, "internal: initializing extension: %s", sqlite3_errmsg(db));
+        fprintf(stderr, "internal: initializing extension: %s\n", sqlite3_errmsg(db));
     } else {
         rc = readAndLoadFile(db, argv[1]);
     }
