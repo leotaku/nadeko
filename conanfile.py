853e6fef4987edd8e18fc40935758c74a9438afc
Leo Gaskin <leo.gaskin@le0.gs>
2022-11-01 14:47:36 +0100

    Commit Conan configuration and corresponding CI build job

---
+++ conanfile.py
@@ -0,0 +1,29 @@
+from conan import ConanFile
+from conan.tools.meson import MesonToolchain, Meson
+from conan.tools.gnu import PkgConfigDeps
+from conan.tools.layout import basic_layout
+
+
+class NadekoConan(ConanFile):
+    name = "nadeko"
+    version = "0.1"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "PkgConfigDeps", "MesonToolchain"
+    exports_sources = "meson.build", "*.c"
+    requires = "sqlite3/3.39.4", "libarchive/3.6.1"
+    tool_requires = "meson/0.63.3", "ninja/1.11.1", "pkgconf/1.9.3"
+
+    def layout(self):
+        basic_layout(self)
+
+    def build(self):
+        meson = Meson(self)
+        meson.configure()
+        meson.build()
+
+    def package(self):
+        meson = Meson(self)
+        meson.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["nadeko", "lines"]
