6ad1676b5b92fa3c210b6aac1dbf9f2d19447f88
Leo Gaskin <leo.gaskin@le0.gs>
2023-09-29 22:03:57 +0200

    Rename pkgconfig dependency to pkg-config

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@
 
 pkgs.llvmPackages_latest.stdenv.mkDerivation {
   name = "dev-shell";
-  nativeBuildInputs = with pkgs; [ clang-tools meson ninja pkgconfig rlwrap ];
+  nativeBuildInputs = with pkgs; [ clang-tools meson ninja pkg-config rlwrap ];
   buildInputs = with pkgs; [ libarchive sqlite ];
   strictDeps = false;
 }
